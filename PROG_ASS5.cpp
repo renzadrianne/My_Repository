f72206e15670ff63bac098e71bb3e3fdbaee1407
Renz Adrianne <41726252+renzadrianne@users.noreply.github.com>
2018-10-27 14:47:58 +0800

    Add files via upload

---
+++ PROG_ASS5.cpp
@@ -0,0 +1,47 @@
+/*
+ PROG_ASS5 ( daily routine)
+ 
+ Name: Salalac, Lorenz Adrianne
+*/
+
+#include <iostream>
+
+using namespace std;
+string time(int x);
+int main() {
+int all[5];
+//all[0]= energy
+all[1]=4;
+all[0]=37;
+all[2]=00;
+int i=1;
+char doing[37] [100];
+while (true) {
+if (all[2]==60) {
+all[2]=00;
+all[1]=all[1]+1;
+} 
+cout<<"by " <<all[1]<<":"<<time(all[2])<<endl;
+cout<<"I am (enter):";
+cin.getline(doing[i],100);
+all[0]= all[0]-1;
+all[2]=all[2]+30;
+i++;
+if (all[0]<=0) {
+cout<<"physical and mental health has approached to zero (0)"<<endl;
+cout <<"too tired!"<<endl;
+cout<<"22:30 , sleeping "<<endl;
+break;
+}
+}
+return 0;
+}
+
+string time(int x) {
+if (x==0) {
+return "00";
+}
+else {
+return "30";
+}
+}
